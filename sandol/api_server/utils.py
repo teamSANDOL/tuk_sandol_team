61176c4fa5939b70d04ee69d65a276ac888de7d4
Seokyoung_Hong <tjrdud0330@naver.com>
2024-04-02 16:24:49 +0900

    add: add_quick_replies function
    api_server의 utils.add_quick_replies 함수를 추가합니다. 이 함수는 카카오톡 응답 형식에 quickRelplies를 추가합니다.

--- sandol/api_server/utils.py
+++ sandol/api_server/utils.py
@@ -4,3 +4,35 @@ def make_textcard(title: str, description: str) -> dict:
         "title": title,
         "description": description
     }
+
+
+def add_quick_replies(output: dict, quick_replies: list) -> dict:
+    """QuickReplies를 추가합니다.
+
+    카카오톡 응답 형식에 알맞게 QuickReplies를 추가합니다.
+    이미 QuickReplies가 존재할 경우에는 그 뒤에 추가합니다.
+    ouput은 version, template 키가 있는 최상위 dict입니다.
+
+    Args:
+        output (dict): 응답 dict로 version, template 키가 있는 최상위 dict입니다.
+        quick_replies (list): 추가할 QuickReplies 리스트로 각 요소는 다음과 같은 dict입니다.
+            {
+                "label": "사용자에게 노출될 바로가기 응답의 표시",
+                "action": "바로가기 응답의 동작" (Message 혹은 Block) ,
+                "messageText": "사용자 측으로 노출될 발화",
+                "blockId": "동작이 Block일 경우 블록 ID" (action이 Block인 경우에만 필수)
+                "extra": "동작이 Block일 경우 추가 정보" (action이 Block인 경우에만 선택)
+            }
+
+    Returns:
+        dict: output에 QuickReplies가 추가된 응답 dict로 json으로 곧바로 반환하면 되는 최종 형태입니다.
+    """
+
+    # output에 quickReplies 키가 없거나 quickReplies가 비어있는 경우 초기화
+    if ("quickReplies" not in output["template"] or
+            not output["template"]["quickReplies"]):
+        output["template"]["quickReplies"] = []
+
+    # quickReplies 추가
+    output["template"]["quickReplies"] += quick_replies
+    return output
