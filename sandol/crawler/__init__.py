b87aff78a9979c16f5f8b818cceca7ba42a20214
Seokyoung_Hong <tjrdud0330@naver.com>
2024-04-11 03:55:08 +0900

    Add: cafeteria_registration.py 추가
    식당의 메뉴 등록을 진행 하는 Restaurant 클래스가 포함된 파일을 추가합니다. 이를 initializing하는 관련 코드도 추가합니다.

--- sandol/crawler/__init__.py
+++ sandol/crawler/__init__.py
@@ -1,1 +1,17 @@
-from .app import Restaurant, get_meals
+"""Sandol Service 팀의 패키지 입니다.
+
+Api팀은 이 패키지를 사용하여 서버 개발을 진행합니다.
+"""
+from .cafeteria_registration import Restaurant as RegistrationRestaurant
+
+
+def get_registration(identification) -> RegistrationRestaurant:
+    """RegistrationRestaurant를 반환하는 함수
+
+    Args:
+        identification (str): 식당을 식별할 수 있는 id
+
+    Returns:
+        RegistrationRestaurant: id에 해당하는 RegistrationRestaurant 객체
+    """
+    return RegistrationRestaurant.by_id(identification)
