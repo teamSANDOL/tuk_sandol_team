07fff8015ab213987638f1211fdd6dcf7d54b0ea
Seokyoung_Hong <tjrdud0330@naver.com>
2024-04-11 03:59:51 +0900

    feat: 애플리케이션 파일 수정 및 식단 정보 등록 API 추가

--- sandol/app.py
+++ sandol/app.py
@@ -1,75 +1,54 @@
-from flask import Flask, jsonify
+"""Sandol의 메인 애플리케이션 파일입니다."""
+from flask import Flask, request
 
-from .api_server import make_textcard
-from .crawler import Restaurant, get_meals
+from sandol.api_server.kakao import KakaoResponse, SimpleTextComponent
+from .crawler import get_registration, RegistrationRestaurant
+from .api_server.kakao import Payload
+
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def root():
+    """루트 페이지입니다."""
     return "Hello Sandol"
 
 
-@app.route("/meal/view")
-def meal_view():
-    """식단 정보를 Carousel TextCard 형태로 반환합니다."""
-    restaurants: list[Restaurant] = get_meals()  # 식당 정보를 가져옵니다.
+@app.post("/meal/register/<meal_type>")
+def meal_register(meal_type: str):
+    """식단 정보를 등록합니다.
 
-    # 출력 형식을 만듭니다.
-    output: dict = {
-        "version": "2.0",
-        "template": {
-            "outputs": []
-        }
-    }
-    carousel = {
-        "type": "TextCard",
-        "items": []
-    }
-
-    on_cam = [{
-        "simpleText": {
-            "text": "교내 식당"
-        }
-    }]
-
-    off_cam = [{
-        "simpleText": {
-            "text": "교외 식당"
-        }
-    }]
-
-    # 식당 정보를 출력 형식에 맞게 변환합니다.
-    for restaurant in restaurants:
-        temp_carousel: dict = carousel.copy()
-
-        # 식단 정보를 TextCard 형태로 변환합니다.
-        lunch = make_textcard(
-            title=f"{restaurant.name}(점심)",
-            description="\n".join(restaurant.lunch)
-        )
-        dinner = make_textcard(
-            title=f"{restaurant.name}(저녁)",
-            description="\n".join(restaurant.dinner)
-        )
-
-        assert isinstance(temp_carousel["items"], list)
-        # Carousel에 Card를 추가합니다.
-        temp_carousel["items"].append(lunch)
-        temp_carousel["items"].append(dinner)
-
-        # Carousel을 출력 형식에 추가합니다.
-        if restaurant.location == "교내":
-            on_cam.append(temp_carousel)
-        elif restaurant.location == "교외":
-            off_cam.append(temp_carousel)
-        else:
-            raise ValueError(f"Invalid location: {restaurant.location}")
-
-    output["template"]["outputs"] = on_cam + off_cam
-
-    return jsonify(output)  # JSON 형태로 반환합니다.
+    중식 및 석식 등록 발화시 호출되는 API입니다.
+    """
+    payload = Payload.from_dict(request.json)
+    restaurant: RegistrationRestaurant = get_registration(payload.user_id)
+    print(request.json)
+    print(restaurant.name)
+    restaurant.clear_menu()
+    restaurant.add_menu(meal_type, payload.detail_params["menu"].origin)
+    print(restaurant.temp_menu, restaurant.lunch, restaurant.dinner)
+    response = KakaoResponse()
+    simpe_text = SimpleTextComponent("식단 정보 등록")
+    return (response + simpe_text).get_json()
 
 
-app.run()
+@app.post("/meal/register_menu")
+def meal_register_menu():
+    """식단 정보를 등록합니다.
+
+    중식 및 석식 등록 발활 이후 메뉴 작성시 호출되는 API입니다.
+    """
+    payload = Payload.from_dict(request.json)
+    restaurant: RegistrationRestaurant = get_registration(payload.user_id)
+
+    restaurant.add_menu("lunch", payload.detail_params["menu"].origin)
+
+    print(restaurant.temp_menu, restaurant.lunch, restaurant.dinner)
+    response = KakaoResponse()
+    simpe_text = SimpleTextComponent("식단 정보 등록")
+    return (response + simpe_text).get_json()
+
+
+if __name__ == "__main__":
+    app.run()
