63043a3fac5d3f5c6c7756210b194db01939b08f
Ivy Ki <144656120+IvyKi@users.noreply.github.com>
2024-04-10 16:54:57 +0900

    fix: __init__.py 수정, settings.py 에러 제거

--- sandol/crawler/__init__.py
+++ sandol/crawler/__init__.py
@@ -1,3 +1,18 @@
 from .cafeteria_view import Restaurant, get_meals
+from .cafeteria_view import Restaurant as ViewRestaurant
+from .cafeteria_registration import Restaurant as RegistrationRestaurant
+
+
+# view 모듈에서 사용자가 직접 호출할 수 있는 함수 정의.
+# 각 점포 별 계정 id로 식별
+def get_view(identification):
+    return ViewRestaurant.by_id(identification)
+
+
+# registration 모듈에서 사용자가 직접 호출할 수 있는 함수 정의
+# 각 점포 별 계정 id로 식별
+def get_registration(identification):
+    return RegistrationRestaurant.by_id(identification)
+
 
 __all__ = ["Restaurant", "get_meals"]
