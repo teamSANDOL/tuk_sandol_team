d2ba060383782510f5615483504678b7fb523be7
Seokyoung-Hong <tjrdud0330@naver.com>
2024-05-14 22:58:04 +0900

    feat: make_meal_card and make_meal_cards function
    식당의 식단 정보를 TextCard 형식으로 반환하는 make_meal_card와 식단 목록에 대해 식단 정보를 Carousel 형식으로 반환하는 함수입니다.

--- sandol/api_server/utils.py
+++ sandol/api_server/utils.py
@@ -1,4 +1,6 @@
 import re
+from .kakao.response.components import CarouselComponent, TextCardComponent
+from ..crawler import RegistrationRestaurant
 
 
 def split_string(s: str) -> list[str]:
@@ -27,3 +29,64 @@ def split_string(s: str) -> list[str]:
     else:
         # white-space를 기준으로 분리하고, 각 항목의 양 끝 공백 제거
         return [item.strip() for item in re.split(r'\s+', s) if item.strip()]
+
+
+def make_meal_card(
+    mealtype: str,
+    restaurant: RegistrationRestaurant,
+    is_temp=False
+) -> TextCardComponent:
+    """식당의 식단 정보를 TextCard 형식으로 반환합니다.
+
+    식당 객체의 식단 정보를 받아 TextCardComponent 객체를 생성하여 반환합니다.
+    만약 is_temp이 True일 경우 임시 메뉴를 사용합니다.
+    만약 메뉴가 없을 경우 "식단 정보가 없습니다."를 반환합니다.
+
+    Args:
+        mealtype (str): "lunch" 또는 "dinner" 중 하나
+        restaurant (RegistrationRestaurant): 식당 정보 객체
+        is_temp (bool, optional): 임시 메뉴를 사용할지 여부. Defaults to False.
+    """
+
+    # is_temp가 True일 경우 임시 메뉴를 호출, 아닐 경우 일반 메뉴를 호출하기 위한 변수
+    meal_attr = f"temp_{mealtype}" if is_temp else mealtype
+
+    # 식사 종류를 한글로 변환하기 위한 딕셔너리
+    mealtype_dict = {"lunch": "점심", "dinner": "저녁"}
+
+    # 카드 제목
+    title = f"{restaurant.name}({mealtype_dict[mealtype]})"  # 식당명(점심)
+
+    # 메뉴 리스트를 개행문자로 연결하여 반환
+    # "메뉴1\n메뉴2\n메뉴3" 또는 "식단 정보가 없습니다."
+    menu_list: list = getattr(restaurant, meal_attr, [])
+    description = "\n".join(
+        menu_list) if menu_list else "식단 정보가 없습니다."
+
+    return TextCardComponent(title=title, description=description)
+
+
+def make_meal_cards(
+    restaurants: list[RegistrationRestaurant], is_temp=False
+) -> tuple[CarouselComponent, CarouselComponent]:
+    """
+    주어진 식당 목록에 대해 각각 점심과 저녁 식단 정보를 CarouselComponent로 반환합니다.
+    카드 생성에는 make_meal_card 함수를 사용합니다.
+    is_temp이 True일 경우 임시 메뉴를 사용합니다.
+
+    Args:
+        restaurants (list[Restaurant]): 식당 정보 리스트
+        is_temp (bool, optional): 임시 메뉴를 사용할지 여부. Defaults to False.
+
+    Returns:
+        tuple[Carousel, Carousel]: 점심 Carousel, 저녁 Carousel
+    """
+    lunch = CarouselComponent()
+    dinner = CarouselComponent()
+
+    # 각 식당에 대해 점심과 저녁 식단 정보를 추가
+    for restaurant in restaurants:
+        lunch.add_item(make_meal_card("lunch", restaurant, is_temp))
+        dinner.add_item(make_meal_card("dinner", restaurant, is_temp))
+
+    return lunch, dinner
