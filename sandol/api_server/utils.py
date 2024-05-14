5a1f46f9eaa76078bf5058a8627c21207558f6e7
Seokyoung_Hong <tjrdud0330@naver.com>
2024-05-15 02:12:38 +0900

    delete: 코드리뷰와 Merge를 위한 불필요 코드 제거

--- sandol/api_server/utils.py
+++
@@ -1,6 +0,0 @@
-def make_textcard(title: str, description: str) -> dict:
-    """TextCard 형식의 JSON을 만들어 반환합니다."""
-    return {
-        "title": title,
-        "description": description
-    }
--- sandol/crawler/__init__.py
+++
@@ -1,1 +0,0 @@
-from .app import Restaurant, get_meals
