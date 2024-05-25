c5e7477e1b821a8e9ef21fe08cfb8452f7316c2b
Ivy Ki <144656120+IvyKi@users.noreply.github.com>
2024-05-25 22:55:37 +0900

    fix: by_dict() classmethod 생성 -> get_meals() 함수 수정

--- sandol/crawler/cafeteria.py
+++ sandol/crawler/cafeteria.py
@@ -1,7 +1,7 @@
 import os
 import json
 import datetime as dt
-from sandol.crawler import settings
+from . import settings
 
 
 class Restaurant:
@@ -20,15 +20,25 @@ class Restaurant:
         self.rest_info()
 
     @classmethod
+    def by_dict(cls, data):
+        """
+        주어진 데이터 딕셔너리로부터 Restaurant 객체를 생성합니다.
+        """
+        registration_time = dt.datetime.fromisoformat(data["registration_time"])
+        class_name = f"{data['name']}"
+        new_class = type(class_name, (Restaurant,), {})
+
+        return new_class(data["name"], data["lunch_menu"], data["dinner_menu"], data["location"], registration_time)
+
+    @classmethod
     def by_id(cls, id_address):
         """
-            식당 별 access id 조회, 식당 이름으로 객체 생성.
-            settings. RESTAURANT_ACCESS_ID : {id : name}
+        식당 별 access id 조회, 식당 이름으로 객체 생성.
+        settings. RESTAURANT_ACCESS_ID : {id : name}
         """
         restaurant_name = settings.RESTAURANT_ACCESS_ID.get(id_address)
 
         if restaurant_name:
-            # test.json : {id:"", name: "", lunch : "" ...}
             current_dir = os.path.dirname(__file__)
             filename = os.path.join(current_dir, 'test.json')
 
@@ -38,17 +48,7 @@ class Restaurant:
                 for restaurant_data in data:
                     # id 검사
                     if restaurant_data["identification"] == id_address:
-                        # 클래스 이름을 각 식당명으로 규정
-                        class_name = f"{restaurant_data['name']}"
-
-                        # 생성된 클래스로 객체를 생성하여 반환
-                        new_class = type(class_name, (Restaurant,), {})
-
-                        registration_time = dt.datetime.fromisoformat(restaurant_data["registration_time"])
-
-                        return new_class(restaurant_data["name"], restaurant_data["lunch_menu"],
-                                         restaurant_data["dinner_menu"], restaurant_data["location"],
-                                         registration_time)
+                        return cls.by_dict(restaurant_data)
 
         else:
             raise ValueError(f"해당 식당을 찾을 수 없습니다. ID: '{id_address}'")
@@ -259,25 +259,14 @@ def get_meals() -> list:
         data = json.load(file)
 
     # 식당 목록 리스트
-    restaurants = []
-
-    for item in data:
-        name = item.get('name', '')
-        lunch = item.get('lunch_menu', [])
-        dinner = item.get('dinner_menu', [])
-        location = item.get('location', '')
-        registration_time = item.get('registration_time', '')
-
-        class_name = f"{item['name']}"
-        new_class = type(class_name, (Restaurant,), {})     # 클래스 이름을 각 식당명으로 규정
-
-        restaurant = new_class(name, lunch, dinner, location, registration_time)
-        restaurants.append(restaurant)                      # 식당 객체 -> 식당 목록 리스트에 추가
+    restaurants = [Restaurant.by_dict(item) for item in data]
 
     return restaurants
 
 
 if __name__ == "__main__":
+    # rests = get_meals()
+    # print(rests)
     identification = "001"      # 001: TIP 가가식당
     rest = Restaurant.by_id(identification)
 
