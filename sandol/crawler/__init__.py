57f1ff786cffcdb417b7e7a8cf369eaf051a3ef7
Ivy Ki <144656120+IvyKi@users.noreply.github.com>
2024-06-08 17:55:19 +0900

    fix: cafeteria.py docstring 수정

--- sandol/crawler/__init__.py
+++ sandol/crawler/__init__.py
@@ -17,14 +17,8 @@ def get_registration(identification) -> Restaurant:
     return Restaurant.by_id(identification)
 
 
-# view 모듈에서 사용자가 직접 호출할 수 있는 함수 정의.
-# 각 점포 별 계정 id로 식별
 def get_view(identification):
     return Restaurant.by_id(identification)
-
-
-# registration 모듈에서 사용자가 직접 호출할 수 있는 함수 정의
-# 각 점포 별 계정 id로 식별
 
 
 __all__ = ["Restaurant", "get_meals"]
