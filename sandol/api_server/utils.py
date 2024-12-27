47aac160edc3aee69eabb4fabc758885227c1def
Seokyoung_Hong <tjrdud0330@naver.com>
2024-12-27 15:43:00 +0900

    fix: check_access_id에서 접근 ID 검증 로직 개선

--- sandol/api_server/utils.py
+++ sandol/api_server/utils.py
@@ -262,9 +262,9 @@ def check_access_id(id_type: str = "restaurant"):
             )
             if (
                 (
-                    id_type == "restaurant" and access_id != RESTAURANT_ACCESS_ID
+                    id_type == "restaurant" and access_id not in RESTAURANT_ACCESS_ID().values()
                 ) or (
-                    id_type == "sandol" and access_id != SANDOL_ACCESS_ID
+                    id_type == "sandol" and access_id != SANDOL_ACCESS_ID().values()
                 )
             ):
                 return JSONResponse(response.get_dict())
